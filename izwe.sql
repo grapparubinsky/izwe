6b1d4612018d2d29e5baf9ca77f7365c0d8a4639
Ruben <ruben@amoruso.me>
2014-12-11 00:04:01 +0100

    aggiunto htaccess e dump struttura db

---
+++ izwe.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 11, 2014 at 12:03 AM
+-- Server version: 5.5.40
+-- PHP Version: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `izwe`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `filla`(IN NumRows INT)
+BEGIN
+        DECLARE i INT;
+        SET i = 1;
+        START TRANSACTION;
+        WHILE i <= NumRows DO
+            INSERT INTO territori VALUES (i,'');
+            SET i = i + 1;
+        END WHILE;
+        COMMIT;
+    END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gruppi`
+--
+
+CREATE TABLE IF NOT EXISTS `gruppi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_gruppo` varchar(100) NOT NULL,
+  `sorvegliante_id` int(11) NOT NULL,
+  `assistente_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `id_2` (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proclamatori`
+--
+
+CREATE TABLE IF NOT EXISTS `proclamatori` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `gruppo_id` int(10) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `cognome` varchar(100) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `gruppo_id` (`gruppo_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=148 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registro`
+--
+
+CREATE TABLE IF NOT EXISTS `registro` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_p` int(10) NOT NULL,
+  `territorio_n` varchar(10) NOT NULL,
+  `data_uscita` date NOT NULL,
+  `data_rientro` date DEFAULT NULL,
+  `registered` bit(1) NOT NULL,
+  `note` mediumtext NOT NULL,
+  `last_edit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `keynice` (`id`,`territorio_n`,`data_uscita`),
+  KEY `territorio_n` (`territorio_n`),
+  KEY `id_p` (`id_p`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=520 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `territori`
+--
+
+CREATE TABLE IF NOT EXISTS `territori` (
+  `id` int(11) NOT NULL,
+  `zona` varchar(255) NOT NULL,
+  `note` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
