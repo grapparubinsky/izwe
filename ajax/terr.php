61647f98003930213f12160986e708f25cc61bf9
Ruben <ruben@amoruso.me>
2015-01-25 14:51:14 +0100

    fix date

--- ajax/terr.php
+++ ajax/terr.php
@@ -31,7 +31,7 @@ $p=$_GET;
 		
 		$ts_data_uscita=date('Y-m-d', strtotime($p['data_uscita'][$i]));
 		
-		if($p['data_rientro'] == '00-00-0000') {
+		if($p['data_rientro'][$i] == '00-00-0000') {
 		$ts_data_rientro='0000-00-00';
 		} else {
 		$ts_data_rientro=date('Y-m-d', strtotime($p['data_rientro'][$i]));
